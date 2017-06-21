926b794f41772a1d3f9821919aa4be591f6ca709
Cameron Napoli <cameronnapoli@gmail.com>
2017-06-21 13:15:19 -0700

    Modularized Yelp Interface

--- yelp.py
+++ yelp.py
@@ -6,14 +6,11 @@ import urllib
 
 class YelpAPIInterface():
     auth_token_url = "https://api.yelp.com/oauth2/token"
-    # NOTE: Your Yelp App client ID here
-    client_id = ""
-    # NOTE: Your Yelp App client secret here
-    client_secret = ""
-
     search_url = "https://api.yelp.com/v3/businesses/search"
 
-    def __init__(self):
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
         return
 
     # POST request to URL which returns an access token
@@ -26,23 +23,29 @@ class YelpAPIInterface():
         return auth_req.json()['access_token']
 
     # GET request with access token in header which fetches API data
-    def getYelpAPIResponse(self):
+    def getYelpAPIResponse(self, params):
         token = self.fetchYelpToken() # fetch token
         if(token == False):
             return ""
 
         oauth_header = {'Authorization': 'Bearer ' + token}
-        params = {
-            "term":"bars",
-            "location":"Irvine,CA",
-            "radius":"5000"
-        }
+
         req_url = self.search_url +"?"+ urllib.parse.urlencode(params)
         print(req_url + "   ") # DEBUG
 
         api_req = requests.get(req_url, headers=oauth_header)
         return api_req.text
 
-y = YelpAPIInterface()
 
-print(y.getYelpAPIResponse())
+YELP_CLIENT_ID = ""
+YELP_CLIENT_SECRET = ""
+
+y = YelpAPIInterface(YELP_CLIENT_ID, YELP_CLIENT_SECRET)
+
+params = {
+    "term":"bars",
+    "location":"Irvine,CA",
+    "radius":"5000"
+}
+
+print(y.getYelpAPIResponse(params))
