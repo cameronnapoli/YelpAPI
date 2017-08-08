a08012d811b31f4b64f1d8b4b3dd4338f23515e3
Cameron Napoli <cameronnapoli@gmail.com>
2017-08-07 20:54:49 -0700

    comments, refactoring, and readme.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # YelpAPI
 
 Script testing out some custom filtering of Yelp data from their V3 API.
+Edit yelp_api_config.txt with your keys and run with python 3 using:
+
+    python3 /path/to/yelp.py
--- yelp.py
+++ yelp.py
@@ -1,29 +1,47 @@
-# Copyright Cameron Napoli 2017
+# Written by: Cameron Napoli, 2017
 # Demo of Yelp API and responses
 
 import requests
 import urllib
 
 class YelpAPIInterface():
+    ''' API interface to make HTTP requests to fetch Yelp data '''
+
     auth_token_url = "https://api.yelp.com/oauth2/token"
     search_url = "https://api.yelp.com/v3/businesses/search"
 
-    def __init__(self, client_id, client_secret):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        return
+    def __init__(self, api_config_file_name):
+        # Read client id & client secret from config file
+        with open(api_config_file_name) as f:
+            for line in f:
+                if 'YELP_CLIENT_ID' in line:
+                    self.client_id = line.split('=')[1].strip(' \n')
+                elif 'YELP_CLIENT_SECRET' in line:
+                    self.client_secret = line.split('=')[1].strip(' \n')
 
-    # POST request to URL which returns an access token
     def fetchYelpToken(self):
+        '''
+        POST request to URL which returns an access token
+
+        :returns: access token from Yelp
+        '''
+
         auth_req = requests.post(self.auth_token_url, data = \
             {'client_id': self.client_id, \
              'client_secret': self.client_secret})
+
         if 'access_token' not in auth_req.json():
             return False
+
         return auth_req.json()['access_token']
 
-    # GET request with access token in header which fetches API data
     def getYelpAPIResponse(self, params):
+        '''
+        GET request with access token in header which fetches API data
+
+        :param params: parameters to be passed into Yelp query
+        :returns: JSON response from Yelp
+        '''
         token = self.fetchYelpToken() # fetch token
         if(token == False):
             return ""
@@ -37,13 +55,11 @@ class YelpAPIInterface():
         return api_req.text
 
 
-YELP_CLIENT_ID = ""
-YELP_CLIENT_SECRET = ""
 
-y = YelpAPIInterface(YELP_CLIENT_ID, YELP_CLIENT_SECRET)
+y = YelpAPIInterface('yelp_api_config.txt')
 
 params = {
-    "term":"bars",
+    "term":"sushi",
     "location":"Irvine,CA",
     "radius":"5000"
 }
